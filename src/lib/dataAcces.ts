240f74c3deb9a0cdcb33d68d9c9b2da69904b038
Franek Marciniak <prostyfranek@protonmail.com>
2024-01-18 01:38:17 +0100

    fix: fix cache

--- src/lib/dataAcces.ts
+++ src/lib/dataAcces.ts
@@ -2,7 +2,9 @@ import { getApiUrl } from "pokedex/server/common";
 import { type GETPokemonsResponse } from "./types";
 
 export const fetchPokemonList = async (searchParams: string) => {
-  const response = await fetch(`${getApiUrl()}/api/pokemons?${searchParams}`);
+  const response = await fetch(`${getApiUrl()}/api/pokemons?${searchParams}`, {
+    cache: "no-store",
+  });
   const data = (await response.json()) as GETPokemonsResponse;
   return data;
 };
