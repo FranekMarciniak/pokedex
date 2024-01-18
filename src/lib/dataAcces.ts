04bf76b4bc26e7df3a054e505935c08846d083d1
Franek Marciniak <prostyfranek@protonmail.com>
2024-01-18 01:33:03 +0100

    fix: :bug: look for a fix

--- src/lib/dataAcces.ts
+++ src/lib/dataAcces.ts
@@ -2,25 +2,7 @@ import { getApiUrl } from "pokedex/server/common";
 import { type GETPokemonsResponse } from "./types";
 
 export const fetchPokemonList = async (searchParams: string) => {
-  try {
-    const response = await fetch(
-      `${getApiUrl()}/api/pokemons?${searchParams}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        cache: "no-store",
-      },
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const data = (await response.json()) as GETPokemonsResponse;
-    return data;
-  } catch (error) {
-    return {
-      result: [],
-      count: 0,
-    };
-  }
+  const response = await fetch(`${getApiUrl()}/api/pokemons?${searchParams}`);
+  const data = (await response.json()) as GETPokemonsResponse;
+  return data;
 };
