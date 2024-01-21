692e2d469b4cf5c9b376c4859462045e04ea8e08
Franek Marciniak <prostyfranek@protonmail.com>
2024-01-21 16:11:15 +0100

    fix: :passport_control: redirect when logedin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,13 @@
+import { getServerAuthSession } from "pokedex/server/auth";
+import { redirect } from "next/navigation";
 import LoginForm from "./LoginForm";
 
-const Page = () => {
+const Page = async () => {
+  const session = await getServerAuthSession();
+  if (session) {
+    return redirect("/");
+  }
+
   return (
     <>
       <h1 className="mb-10 mt-4 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,13 @@
+import { getServerAuthSession } from "pokedex/server/auth";
 import RegisterForm from "./RegisterForm";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const session = await getServerAuthSession();
+  if (session) {
+    return redirect("/");
+  }
+
   return (
     <>
       <h1 className="mb-10 mt-4 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
